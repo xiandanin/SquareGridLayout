b34b2e82a1e64e74614d8ef25bf035aef70d4f35
dengyuhan <464958567@qq.com>
2018-07-13 20:28:49 +0800

    Initial commit

---
+++ square-gridlayout/src/main/java/com/dyhdyh/view/squaregrid/SquareGridLayout.java
@@ -0,0 +1,31 @@
+package com.dyhdyh.view.squaregrid;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.ViewGroup;
+
+
+/**
+ * @author dengyuhan
+ *         created 2018/7/13 20:27
+ */
+public class SquareGridLayout extends ViewGroup {
+
+    public SquareGridLayout(Context context) {
+        super(context);
+    }
+
+    public SquareGridLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public SquareGridLayout(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+
+    @Override
+    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+
+    }
+}
